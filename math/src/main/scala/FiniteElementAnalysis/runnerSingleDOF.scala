c9ebe4fee127ec52b48f09a3e1d700ec86b9cdde
fletchwsf <wsf.fletcher@gmail.com>
2017-06-11 19:54:23 -0400

    Create a runner method to coordinate the analysis of system with a single degree-of-freedom.

---
+++ math/src/main/scala/FiniteElementAnalysis/runnerSingleDOF.scala
@@ -0,0 +1,46 @@
+package FiniteElementAnalysis
+
+/**
+  * Created by wsf on 6/11/2017.
+  */
+object runnerSingleDOF {
+
+
+  def main(args: Array[String]): Unit = {
+
+    // file name from command line
+
+    // read the input file into memory
+
+    // read the degree-of-freedom value for the model
+
+    // read the number of elements in the model
+
+    // read the list of nodes with single point constraints
+
+    // read the element connection table matrix
+
+    // read the element cross-sectional area table
+
+    // read the element length table
+
+    // read the element modulus of elasticity table
+
+    // read the element body force table
+
+    // read the multi-point constraint array matrix
+
+    // build the initial stiffness matrix array
+
+    // establish the penalty stiffness value
+
+    // add the single-point constraint values to the stiffness matrix array
+
+    // add the multi-point constraint values to the stiffness matrix array
+
+    // solve for the element displacement vector
+
+    // solve for the element stress vector
+
+  }
+}
