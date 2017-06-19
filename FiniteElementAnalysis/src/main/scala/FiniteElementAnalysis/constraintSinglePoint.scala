ee39293646d706d25bcf9fc30addf8cfb1756ef9
fletchwsf <wsf.fletcher@gmail.com>
2017-06-18 21:03:46 -0400

    move to new folder

---
+++ FiniteElementAnalysis/src/main/scala/FiniteElementAnalysis/constraintSinglePoint.scala
@@ -0,0 +1,20 @@
+package FiniteElementAnalysis
+
+/**
+  * Created by wsf on 6/10/2017.
+  */
+object constraintSinglePoint {
+
+
+  def add(
+                                 kMatrix : Array[Array[Double]],
+                                 constraints : Array[Int]
+                                 ): Array[Array[Double]] = {
+
+    val C : Double  = matrix.max(kMatrix) * 10000.0
+
+    for(i <- constraints.indices)
+      kMatrix(constraints(i))(constraints(i)) += C
+    kMatrix
+  }
+}
